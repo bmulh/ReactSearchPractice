dba47443b99fc40ba38779d251db331a2aa7eec4
Brian Mulh <mulh.brian@gmail.com>
2018-06-21 20:02:38 -0400

    video list changes

--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
-import React from 'react'; //go get React from install modules and give access to this file
-import ReactDOM from 'react-dom'; //used to render to DOM
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar';
-
-const API_KEY = 'AIzaSyA6JMEFrrmADZjzIN5K-CYNgBR9xizGDS0';
-
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
-    console.log(data);
-});
-
-const App = () => {
-    return (
-    <div>
-        <SearchBar/>
-    </div>
+import VideoList from './components/video_list';
+const API_KEY = 'AIzaSyCipiM9B29wX55DG0l6vYIih80xvgEJT1M';
+class App extends Component {
+  constructor(props) {
+        super(props);
+        this.state = { videos: [] };
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+        this.setState({ videos });
+        });
+    }
+    render() {
+        return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
     );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
